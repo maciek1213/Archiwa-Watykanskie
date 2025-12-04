68d5e39bc7c5d75fb18f5e4db06f04c1c6064b87
maciek1213 <msawiec@gmail.com>
2025-12-05 00:46:14 +0100

    Started working on user class

---
+++ backend/src/main/java/user/UserController.java
@@ -0,0 +1,15 @@
+package user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+}
