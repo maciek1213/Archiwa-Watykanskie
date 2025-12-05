befc1c8a6d2fb21c8ea397043d4d399d716a1caa
Maciej <msaw@student.agh.edu.pl>
2025-12-05 18:17:15 +0100

    Added basic logic for user

--- backend/src/main/java/user/User.java
+++ backend/src/main/java/user/User.java
@@ -26,4 +26,14 @@ public class User {
     @Column(unique = true)
     private String phoneNumber;
 
+    public User(String  firstName, String lastName, String email, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public User() {
+
+    }
 }
--- backend/src/main/java/user/UserController.java
+++ backend/src/main/java/user/UserController.java
@@ -1,5 +1,9 @@
 package user;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +15,17 @@ public class UserController {
 
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<User> createUser(@RequestBody UserDTO user) {
+        User newUser = userService.createUser(
+            user.getFirstName(),
+            user.getLastName(),
+            user.getEmail(),
+            user.getPhoneNumber()
+        );
+
+        return  new ResponseEntity<>(newUser, HttpStatus.CREATED);
     }
 }
