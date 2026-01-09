f3bdba815db5eab89f6fbb18c41c34e1b83e8505
Grzegorz Rybiński <grrybinski@gmail.com>
2026-01-09 01:54:11 +0100

    category problem solved

--- backend/src/main/java/pl/agh/edu/libraryapp/book/Category.java
+++ backend/src/main/java/pl/agh/edu/libraryapp/book/Category.java
@@ -1,14 +1,17 @@
 package pl.agh.edu.libraryapp.book;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 @Table(name = "category")
+@JsonIgnoreProperties({"books"})  // Ignoruj pole books przy deserializacji
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,16 +24,43 @@ public class Category {
     @JsonIgnoreProperties("categories")
     private Set<Book> books = new HashSet<>();
 
-    public Category() {}
+    public Category() {
+    }
 
     public Category(String name) {
         this.name = name;
     }
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public Set<Book> getBooks() { return books; }
-    public void setBooks(Set<Book> books) { this.books = books; }
+    @JsonCreator
+    public Category(
+            @JsonProperty("id") Long id,
+            @JsonProperty("name") String name
+    ) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
 }
